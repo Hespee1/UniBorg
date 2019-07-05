1776563b0c4eddc4e74e989b39cb5f406371fd75
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-07-05 15:19:34 -0700

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -49,7 +49,7 @@ async def _(event):
     packname = f"@loxxi Pack"
     packshortname = f"loxxi_packs"  # format: Uni_Borg_userid
 
-    await event.edit("Processing this sticker. Please Wait!")
+    await event.edit("Grabbing Sketch Brush & Painting this Picture..!")
 
     async with borg.conversation("@Stickers") as bot_conv:
         now = datetime.datetime.now()
@@ -110,7 +110,7 @@ async def _(event):
                 await silently_send_message(bot_conv, sticker_emoji)
                 await silently_send_message(bot_conv, "/done")
 
-    await event.edit(f"sticker added! Your pack can be found [here](t.me/addstickers/{packshortname})")
+    await event.edit(f"Painting Done! Find Your Masterpiece [Here](t.me/addstickers/{packshortname})")
 
 
 @borg.on(admin_cmd("packinfo"))
