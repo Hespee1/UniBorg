29e937969fab9ceb6eed0c1133fc4160524da635
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-07-10 16:34:49 +0100

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -106,7 +106,7 @@ class Config(object):
     os.makedirs(TMP_DOWNLOAD_DIRECTORY)
     t_file = open(TMP_DOWNLOAD_DIRECTORY+"auth_token.txt","w")
     t_file.write(AUTH_TOKEN_DATA)
-    t_file.close()
+    t_file.close
 
 class Production(Config):
     LOGGER = False
