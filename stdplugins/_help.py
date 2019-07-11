6e88b1ffefdabedbe5a99b232d59ec409568a4ee
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-07-11 01:25:28 +0100

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -12,7 +12,7 @@ async def _(event):
         s_help_string = borg._plugins[splugin_name].__doc__
     else:
         s_help_string = ""
-    help_string = """@UniBorg(**Custom Built By** @loxxi) \n**Verified Account**: ✅\n**Official Website**: https://webloaded.com.ng\n
+    help_string = """@UniBorg ( **Custom Built By** @loxxi ) \n**Verified Account**: ✅\n**Official Website**: https://webloaded.com.ng\n
 Python {}
 Telethon {}
 UserBot Forked from https://github.com/somto811/uniborg""".format(
