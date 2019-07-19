ae506d3133cc518358e2fa5bf8eabc87f683101f
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-07-18 17:01:27 -0700

    Update preview.py

--- stdplugins/preview.py
+++ stdplugins/preview.py
@@ -4,7 +4,7 @@ Reply to a message with .p to toggle the webpage preview of a message
 from telethon.errors import MessageNotModifiedError
 from telethon.tl.functions.messages import EditMessageRequest
 
-from stdplugins.kbass_core import self_reply_cmd
+
 
 
 @self_reply_cmd(borg, r"^\.p$")
