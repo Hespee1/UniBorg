4ae8bd5fff1b2d4ea937063f977c04d363356a7d
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-07-21 22:35:53 +0100

    Update unsupported.py

--- stdplugins/unsupported.py
+++ stdplugins/unsupported.py
@@ -74,8 +74,7 @@ Files Batch Uploader Plugin for userbot.
 usage:- .upb 
 Note:- set TEMP_DIR in Your ENV Vars First.
 By:-@Zero_cool7870	
-"""
-import os 
+"""import os 
 import asyncio
 from uniborg.util import admin_cmd
 from telethon import events
