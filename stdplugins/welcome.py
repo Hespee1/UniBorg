19dd61d3f38844ca4b221ee5d5232dde37f97b28
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-07-21 23:18:22 +0100

    Update welcome.py

--- stdplugins/welcome.py
+++ stdplugins/welcome.py
@@ -19,7 +19,7 @@ async def _(event):
         user_joined=True,
         user_left=False,
         user_kicked=False,
-        link_preview=False"""
+        link_preview=False, """
         if event.user_joined:
             if cws.should_clean_welcome:
                 try:
