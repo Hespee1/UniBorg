e742b35900122a39baacd76d9d7a3a2382352cc3
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-07-23 22:50:47 +0100

    Update ping.py

--- stdplugins/ping.py
+++ stdplugins/ping.py
@@ -8,7 +8,8 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    await event.edit("Pong!")
+    await event.edit("Ping is High AF 😭")
     end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    await event.edit("Pong!\n{}".format(ms))
+    ms = (end - start).microseconds/1000
+    await event.edit("Ping is High AF 😭\n{}".format(ms))
+© 2019 GitHub, Inc.
