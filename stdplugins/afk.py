91da5b94cadb3a20f6d5a9250ed6d1b877b78680
Somtochukwu Ernest <52139555+Somto811@users.noreply.github.com>
2019-07-30 20:45:02 +0100

    Made some changes

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -100,14 +100,10 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"`Hi there.` " + \
-            f"`I'm afk`.\n__Sebebi:__ {reason}" \
+        message_to_reply = f"My Boss is Away From Keyboard . \nHe Will Check Your Messeges and Try to Reply.\nKindly Don't Spam . Thank You .\n\n**Last Seen: Only God Knows.** " + \
+            f"\n\n__Reason:__ {reason}" \
             if reason \
-            else f"I'm currently away from keyboard since {afk_since}. Meanwhile, you can spam admins here with Noods in PM.(◕‿◕)  Until I come back to check your USELESS messages. "
-        #message_to_reply = f"`I'm currently away from keyboard since` {afk_since} " + \
-            #f"`And I will be right back soon`.\n__Reason:__ {reason}" \
-            #if reason \
-            #else f"I'm currently away from keyboard since {afk_since}. Meanwhile, you can spam admins here with Noods in PM.(◕‿◕)  Until I come back to check your USELESS messages. "        
+            else f"/kickme\n\n**Important Notice**\n\n[This User Is Ded Forever...](https://telegra.ph//file/a53fa950ff31781d5930a.jpg) "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in borg.storage.last_afk_message:  # pylint:disable=E0602
